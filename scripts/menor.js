7f67acb77993b91261e69095cc30dd6cd30537cc
Menor25 <66224353+Menor25@users.noreply.github.com>
2020-06-05 19:47:49 +0100

    Update menor.js

--- scripts/menor.js
+++ scripts/menor.js
@@ -1,13 +1,17 @@
 
-function menor(){
-    var fullName = "Theophilus Ajiri Menor"; 
-    var hngId = "HNG-02809";
-    var langauge = "HTML, CSS";
-    var email = "ajirimenor@aim.com"
 
-    var myOutput = "Hello World, i am " + fullName + " with HNGi7 ID " + hngId + " and email " + email + " using " + langauge + " for stage 2 task";
+var fullName = "Theophilus Ajiri Menor"; 
+var hngId = "HNG-02809";
+var langauge = "javascript";
+var email = "ajirimenor@gmail.com";
+
+var myOutput = "Hello World, i am " + fullName + " with HNGi7 ID " + hngId + " and email " + email + " using " + langauge + " for stage 2 task";
+
+function menorOutput(){
+    
    
     return myOutput;
 }
 
-console.log(menor());
+console.log(myOutput);
+menorOutput();
